400df0c44e54df364eb9fde9c9df5f9d5edb22eb
Naren2694 <101439697+Naren2694@users.noreply.github.com>
2022-04-03 13:55:53 +0530

    Create Sample.java

---
+++ Sample.java
@@ -0,0 +1,14 @@
+
+public class Firstproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Naren");
+		System.out.println("dran");
+		System.out.println("dummy");
+		
+		
+	}
+
+}
